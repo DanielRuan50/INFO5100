45ed51dd6afd3b3ed5cf2273710e6a484db447ab
DanielRuan50 <yining50@gmail.com>
2022-10-14 11:00:23 -0700

    Update Lab4

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         //create an object of WeightedGrades class
         WeightedGrades calculate = new WeightedGrades();
 
-        //prompt users to enter Total point total of assignments123123123123
+        //prompt users to enter Total point total of assignments
         System.out.print("Total point total of assignments (split by space): \n");
 
         String res = scanner.nextLine();
